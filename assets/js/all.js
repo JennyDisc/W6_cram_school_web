cd4315e4e9c0fcbc682f583dcdc3f8bcdb2c794f
JennyWu <touki741@hotmail.com>
2022-09-14 13:07:20 +0800

    Update 2022-09-14T05:07:15.904Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,36 +1,35 @@
+"use strict";
+
 // Swiper程式清單跑馬單
 var swiper = new Swiper('.course-swiper', {
   // 自動撥放
   autoplay: {
-      disableOnInteraction: false,
-      delay: 0
+    disableOnInteraction: false,
+    delay: 0
   },
   speed: 1500,
   loop: true,
   // 卡片元素的間隔 
   spaceBetween: 16,
-//   centeredSlides: true,
+  //   centeredSlides: true,
   // "auto"值會根據 swiper-slide 寬度自動調整畫面出現的數量 
-  slidesPerView: "auto",
-});
+  slidesPerView: "auto"
+}); // Swiper心得分享
 
-
-// Swiper心得分享
-const feedbackswiper = new Swiper('.feedback-swiper', {
+var feedbackswiper = new Swiper('.feedback-swiper', {
   // Optional parameters
   effect: 'fade',
   fadeEffect: {
     crossFade: true
   },
-  
   // If we need pagination
   pagination: {
-    el: '.swiper-pagination',
+    el: '.swiper-pagination'
   },
-
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+    prevEl: '.swiper-button-prev'
+  }
 });
+//# sourceMappingURL=all.js.map
